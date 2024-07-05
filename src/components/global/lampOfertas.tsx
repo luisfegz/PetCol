9517240ffe4825d477bfafcab62e9e67ddbe14c2
Luis Felipe <luisfelipegaitanzambrano@gmail.com>
2024-07-04 21:55:49 -0500

    lampOfertas update

--- src/components/global/lampOfertas.tsx
+++ src/components/global/lampOfertas.tsx
@@ -5,7 +5,7 @@ import { cn } from '@/lib/utils'
 import { SparklesCore } from './sparkles'
 
 
-export function LampComponentJuguete() {
+export function LampComponentAccesorios() {
   return (
     <LampContainer>
       <motion.h1
@@ -16,9 +16,9 @@ export function LampComponentJuguete() {
           duration: 0.8,
           ease: 'easeInOut',
         }}
-        className="mt-20 bg-gradient-to-br from-neutral-300 to-neutral-500 py-4 bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-7xl"
+        className="mt-[408px] md:mt-[340px] bg-gradient-to-br from-neutral-300 to-neutral-500 py-4 bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-7xl"
       >
-        Aprovecha los descuentos
+        Aprovecha los descuentos         
         <br /> de fin de temporada :&apos;)
       </motion.h1>
     </LampContainer>
@@ -35,7 +35,7 @@ export const LampContainer = ({
   return (
     <div
       className={cn(
-        'relative flex min-h-[800px] flex-col items-center justify-center overflow-hidden bg-neutral-950 w-full rounded-md z-0',
+        'relative flex min-h-[800px] flex-col items-center justify-center overflow-hidden bg-neutral-950 w-full z-0',
         className
       )}
     >
@@ -110,7 +110,7 @@ export const LampContainer = ({
         <div className="absolute inset-auto z-40 h-44 w-full -translate-y-[12.5rem] bg-neutral-950 "></div>
       </div>
 
-      <div className="relative z-50 flex -translate-y-80 flex-col items-center px-5">
+      <div className="relative z-50 flex md:-translate-y-[480px] -translate-y-[550px] flex-col items-center px-5">
         {children}
       </div>
     </div>
