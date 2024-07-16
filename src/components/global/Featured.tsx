d3b0092b2f2a6ca38d003170942c308c288bd853
Luis Felipe <luisfelipegaitanzambrano@gmail.com>
2024-07-15 22:19:08 -0500

    button cartIcon

--- src/components/global/Featured.tsx
+++ src/components/global/Featured.tsx
@@ -18,20 +18,24 @@ function Featured({}: Props) {
                     {/* <------------------------------------------------------------ Button Read More REMEMBER ---------------------------------------------------------------------------------------------------------------------> */}
                         <WrapperBtn>
                             <div className='mt-6 space-x-1.5 md:space-x-4'>
-                                <button className="relative inline-flex h-10 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
-                                    <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
-                                    <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-light md:font-medium text-white backdrop-blur-3xl">
-                                        Leer más
-                                    </span>
+                                <button className="inline-flex h-8 text-sm animate-shimmer items-center justify-center z-40
+                                        rounded-md border border-zinc-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] 
+                                        bg-[length:200%_100%] px-1.5 font-normal md:font-medium text-slate-100 transition-colors focus:outline-none 
+                                        focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50
+                                    "
+                                >
+                                    Leer más
                                 </button>
                             </div>         
                         {/* <------------------------------------------------------------ Button with functions of add to cart REMEMBER -----------------------------------------------------------------------------------------------> */}
                             <div className='mt-6 space-x-1.5 md:space-x-4'>
-                                <button className="relative inline-flex h-10 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
-                                    <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
-                                    <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-light md:font-medium text-white backdrop-blur-3xl">
-                                        Añadir<CartIcon />
-                                    </span>
+                                <button className="inline-flex h-8 text-sm animate-shimmer items-center justify-center z-40
+                                        rounded-md border border-zinc-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] 
+                                        bg-[length:200%_100%] px-1.5 font-normal md:font-medium text-slate-100 transition-colors focus:outline-none 
+                                        focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50
+                                    "
+                                >
+                                    Añadir <CartIcon /> 
                                 </button>
                             </div>         
                     {/* <-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------> */}      
